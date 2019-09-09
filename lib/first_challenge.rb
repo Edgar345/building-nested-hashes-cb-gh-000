5c364fddcab483bb672dcc6779d9e19e19cc90e8
Edgar V <evenegas9@ucmerced.edu>
2019-09-09 22:54:21 +0000

    Automatically backed up by Learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -35,7 +35,7 @@ def first_challenge
        },
        heroine_friends: [
          {name: "Steven", age: "30", attitude: "confused"},
-          {name: "Nurse", age: "44", attitude: "worried"}
+         {name: "Nurse", age: "44", attitude: "worried"}
        ]
      }
    }
